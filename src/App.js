dd934de151d4ac5c08d866020000708a99dd0526
NikolasPlatonov <platonov.nikolas@gmail.com>
2021-02-03 15:12:20 +0200

    Simple calendar page markup added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,44 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>HELLO WORLD!</div>
-      <div>HELLO WORLD!</div>
-      <div>HELLO WORLD!</div>
+    <div className="container">
+      <div className="content_container">
+        <div className="header_container">
+          <div className="title_container">Calendar</div>
+          <div className="buttons_container">
+            <div>Button_1</div>
+            <div>Button_2</div>
+          </div>
+        </div>
+        <div className="calendar_container">
+          <div className="table_header">
+            <div className="table_header_item">Name</div>
+            <div className="table_header_item">Mon</div>
+            <div className="table_header_item">Tue</div>
+            <div className="table_header_item">Wed</div>
+            <div className="table_header_item">Thu</div>
+            <div className="table_header_item">Fri</div>
+          </div>
+          <div className="table_content">
+            <div className="time_name_column">
+              <div>10:00</div>
+              <div>11:00</div>
+              <div>12:00</div>
+              <div>13:00</div>
+              <div>14:00</div>
+              <div>15:00</div>
+              <div>16:00</div>
+              <div>17:00</div>
+              <div>18:00</div>
+            </div>
+            <div>1 col</div>
+            <div>2 col</div>
+            <div>3 col</div>
+            <div>4 col</div>
+            <div>5 col</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
