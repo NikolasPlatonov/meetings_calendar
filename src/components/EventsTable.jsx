1ac36d1ee994c79caa1e081b53e6f13da21f9e97
NikolasPlatonov <platonov.nikolas@gmail.com>
2021-02-05 17:53:42 +0200

    Markup for events table rendered

--- src/components/EventsTable.jsx
+++ src/components/EventsTable.jsx
@@ -1,31 +1,57 @@
-import React, {useState} from 'react';
+import React from 'react';
 import data from '../data/data.json';
 
 export const EventsTable = () => {
-  const [state, setAppState] = useState(data);
-  console.log("EventsTable_state_2", state)
-  // const tableDaysTitles = ['Name', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
-  const tableTimeTitles = ['Name', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
+  console.log("EventsTable_data", data)
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+  const hoursOfDay = ['10', '11', '12', '13', '14', '15', '16', '17', '18'];
+
+  const events = (data, days) => {
+    for (let key in data) {
+      for (let day of days) {
+        if (key.slice(0, 3) === day) {
+          return (data[key]);
+        }
+      }
+    }
+  };
+
+  const dayFilter = (hoursArr, day) => {
+    const result = hoursArr.map(hour => {
+      return (
+        <div className="time_title_item" key={day + hour}>
+          {day} {hour}
+        </div>
+      )
+    })
+    return result;
+  }
+
   return (
     <div className="calendar_container">
+
       <div className="time_name_column">
-        {tableTimeTitles.map(time => {
+        <div className="time_title_item">Name</div>
+        {hoursOfDay.map(time => {
           return <span className="time_title_item" key={time}>{time}</span>;
         })}
-
-
       </div>
 
       <div className="table_content">
 
+        <div className="table_day_header">
+          {daysOfWeek.map(day => {
+            return <span className="time_title_item" key={day}>{day}</span>;
+          })}
+        </div>
 
+        <div className="table_day_events" >
 
+          {daysOfWeek.map(day => {
+            return (<div key={day}>{dayFilter(hoursOfDay, day)}</div>)
+          })}
 
-
-        {/* {state[0].eventTime.map(time => {
-            return <span key={Object.keys(time)}>{Object.keys(time)}</span>;
-          })} */}
-
+        </div>
       </div>
     </div>
   );
