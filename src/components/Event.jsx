8ef69a753a28bc3f190d7dfc629541fb582a9fbd
NikolasPlatonov <platonov.nikolas@gmail.com>
2021-02-10 12:36:57 +0200

    Delete event btn added CSS changed

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const Event = ({title, day, time}) => {
+export const Event = ({title, day, time, deleteEvent}) => {
 
   return (
     <div className="event_container">
       <div className="event_title">{title}</div>
-      <div className="event_delete">
+      <div className="event_delete" onClick={() => {deleteEvent(day, time)}}>
         &#10006;
       </div>
     </div>
--- src/components/EventsTable.jsx
+++ src/components/EventsTable.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import _ from 'lodash';
 import data from '../data/data.json';
 import {Event} from './Event';
 
 export const EventsTable = () => {
+  const [state, setState] = useState(data);
+  console.log("1_STATE===", state)
 
-  console.log("EventsTable_data", data)
   const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
   const hoursOfDay = ['10', '11', '12', '13', '14', '15', '16', '17', '18'];
 
+  const deleteEvent = (day, time) => {
+    let newState = _.cloneDeep(state);
+    _.unset(newState, day + '.' + time)
+    setState(newState)
+  }
+
   const dayFilter = (data, hoursArr, day) => {
-    const result = hoursArr.map(hour => {
+    let result = hoursArr.map(hour => {
       return (
-        <div className="time_title_item" key={day + hour}>
-          {data[day] && data[day][hour] ? <Event title={data[day][hour]['title']} day={day} time={hour} /> : ''}
+        <div className="event_item" key={day + hour}>
+          {data[day] && data[day][hour] ?
+            <Event title={data[day][hour]['title']} day={day} time={hour} deleteEvent={deleteEvent} /> :
+            ''}
 
         </div>
       )
@@ -25,9 +35,9 @@ export const EventsTable = () => {
     <div className="calendar_container">
 
       <div className="time_name_column">
-        <div className="time_title_item">Name</div>
+        <div className="time_day_title_item">Name</div>
         {hoursOfDay.map(time => {
-          return <span className="time_title_item" key={time}>{time}</span>;
+          return <span className="time_day_title_item" key={time}>{time}:00</span>;
         })}
       </div>
 
@@ -35,14 +45,14 @@ export const EventsTable = () => {
 
         <div className="table_day_header">
           {daysOfWeek.map(day => {
-            return <span className="time_title_item" key={day}>{day.slice(0, 3)}</span>;
+            return <span className="time_day_title_item" key={day}>{day.slice(0, 3)}</span>;
           })}
         </div>
 
         <div className="table_day_events" >
 
           {daysOfWeek.map(day => {
-            return (<div key={day}>{dayFilter(data, hoursOfDay, day)}</div>)
+            return (<div key={day}>{dayFilter(state, hoursOfDay, day)}</div>)
           })}
 
         </div>
